660770190381bb9536279eee40a5a393f3b44396
Hunter Nunnery <hunter.nunnery@yahoo.com>
2019-04-22 11:14:24 -0500

    Update careers.vue

--- pages/careers.vue
+++ pages/careers.vue
@@ -12,11 +12,39 @@ export default {
   components: {
     Jobs
   },
-  data() {
-    return {};
-  },
-  metaInfo: {
-    titleTemplate: "%s | Careers"
+  head() {
+    return {
+      titleTemplate: "%s Careers",
+      meta: [
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: "Stealth Entry Careers"
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content:
+            "Stealth Entry Cyber Security Solutions is hiring! We have a diverse portfolio of contracts positions."
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content:
+            "https://res.cloudinary.com/missionwebdev/image/upload/f_auto/v1549812205/StealthEntry/SE_-_Apply_today.png"
+        },
+        {
+          hid: "og:url",
+          property: "og:url",
+          content: "https://www.stealthentry.com/careers"
+        },
+        {
+          hid: "twitter:card",
+          name: "twitter:card",
+          content: "summary_large_image"
+        }
+      ]
+    };
   }
 };
 </script>
